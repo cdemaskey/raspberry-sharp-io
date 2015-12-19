0aeaaf462a847ec92beb6f00254e025ee824d21d
Christoph DeMaskey <christoph.demaskey@gmail.com>
2015-12-19 00:04:34 -0500

    update the nuspec version to alpha2. Added AssemblyInformationalVersion to the shared assembly info with the same version as the nuget package.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -27,3 +27,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyFileVersion("2.3.0.0")]
+[assembly: AssemblyInformationalVersion("2.3.0-alpha2")]
